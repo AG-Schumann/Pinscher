f43276446b16463780f72ca80a53487be76702ec
jarongrigat <jaron.grigat@googlemail.com>
2019-09-04 15:23:40 +0200

    Better debugging

---
+++ src/main/java/stormcontrol/InfluxBolt.java
@@ -0,0 +1,56 @@
+package stormcontrol;
+
+import org.apache.storm.task.OutputCollector;
+import org.apache.storm.task.TopologyContext;
+import org.apache.storm.topology.OutputFieldsDeclarer;
+import org.apache.storm.topology.base.BaseRichBolt;
+import org.apache.storm.tuple.Fields;
+import org.apache.storm.tuple.Tuple;
+import org.apache.storm.tuple.Values;
+import org.influxdb.InfluxDB;
+import org.influxdb.InfluxDBFactory;
+import org.influxdb.dto.Point;
+
+import java.util.Date;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+public class InfluxBolt extends BaseRichBolt {
+
+	private InfluxDB influxDB;
+	private OutputCollector collector;
+
+	@Override
+	public void prepare(Map<String, Object> map, TopologyContext topologyContext,
+			OutputCollector outputCollector) {
+		collector = outputCollector;
+		influxDB = InfluxDBFactory.connect("http://localhost:8086");
+		// this should be imported from settings.json in the future
+		influxDB.setDatabase("testing_data");
+	}
+
+	@Override
+	public void execute(Tuple input) {
+		String source = input.getSourceComponent();
+		System.out.println(source);
+		String topic = input.getStringByField("topic");
+		String[] parts = topic.split("__");
+		String sensor_name = parts[0];
+		String reading = parts[1];
+        String quantity = input.getStringByField("quantity");
+		Point point = Point.measurement(quantity)
+				.time(input.getDoubleByField("timestamp").longValue(), TimeUnit.MILLISECONDS)
+				.tag("sensor_name", sensor_name).addField(reading, input.getDouble(2))
+				.build();
+		influxDB.write(point);
+		// System.out.println("WROTE POINT TO DATABASE" + sensor_name + " " + type + " @
+		// " + new Date(input.getDoubleByField("timestamp").longValue()));
+		collector.emit(input, new Values("end"));
+		collector.ack(input);
+	}
+
+	@Override
+	public void declareOutputFields(OutputFieldsDeclarer declarer) {
+		declarer.declare(new Fields("end"));
+	}
+}
---
+++ src/main/java/stormcontrol/IntegralBolt.java
@@ -0,0 +1,66 @@
+package stormcontrol;
+
+import org.apache.storm.task.OutputCollector;
+import org.apache.storm.task.TopologyContext;
+import org.apache.storm.topology.OutputFieldsDeclarer;
+import org.apache.storm.topology.base.BaseWindowedBolt;
+import org.apache.storm.tuple.Fields;
+import org.apache.storm.tuple.Tuple;
+import org.apache.storm.tuple.Values;
+import org.apache.storm.windowing.TupleWindow;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class IntegralBolt extends BaseWindowedBolt {
+
+	private OutputCollector collector;
+
+	@Override
+	public void prepare(Map<String, Object> topoConf, TopologyContext context,
+            OutputCollector collector) {
+		this.collector = collector;
+	}
+
+	@Override
+	public void execute(TupleWindow inputWindow) {
+
+		List<Tuple> tuples = inputWindow.get();
+        System.out.println("TUPLES: " + tuples);
+		Double setpoint = tuples.get(tuples.size() - 1).getDoubleByField("setpoint");
+        double integral = 0.0;
+		// create lists of timestamps and values in window from now to now - delta_t
+		List<Double> timestamps = new ArrayList<Double>();
+		List<Double> values = new ArrayList<Double>();
+        Double delta_t = tuples.get(tuples.size() - 1).getDoubleByField("dt_int");
+		Double t0 = System.currentTimeMillis() - delta_t * 1000;
+		for (Tuple tuple : tuples) {
+			Double t = tuple.getDoubleByField("timestamp");
+			if (t >= t0) {
+				timestamps.add(t / 1000);
+				values.add(tuple.getDoubleByField("value") - setpoint);
+			}
+		}
+        System.out.println("TIMESTAMPS: " + timestamps);
+
+		// integral from t0 to first data point in window
+		integral += (timestamps.get(0) - t0 / 1000) * values.get(0);
+
+		// integral over the data points (trapezoidal rule)
+		for (int i = 0; i < tuples.size() - 1; ++i) {
+			integral += 0.5 * (timestamps.get(i + 1) - timestamps.get(i))
+					* (values.get(i + 1) + values.get(i));
+		}
+		System.out.println("INTEGRAL: " + integral);
+        collector.emit(new Values(tuples.get(0).getStringByField("topic"),
+				tuples.get(tuples.size() - 1).getDoubleByField("timestamp"), integral,
+				"integral", tuples.get(tuples.size() - 1).getStringByField("key")));
+
+	}
+
+	@Override
+	public void declareOutputFields(OutputFieldsDeclarer declarer) {
+		declarer.declare(new Fields("topic", "timestamp", "integral", "type", "key"));
+	}
+}
